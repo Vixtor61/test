9809a5822855a65f0eede580b7f4262525a99dc6
Vixtor61 <00198117@uca.edu.sv>
2018-11-19 09:31:01 -0600

    changes

---
+++ public/javascript/pass.js
@@ -0,0 +1,15 @@
+let pass = {
+    init: function () {
+        this.addEvents();
+    },
+    addEvents: function () {
+        let reg = document.querySelector('#password')
+        let reg1 = document.querySelector('#cpass')
+        
+        script(src="/javascripts/main.js")
+        console.log(reg);
+        
+        
+    }
+};
+window.onload = () => pass.init();
